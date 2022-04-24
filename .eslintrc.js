7f55140a93b24a20339b8ae0809668fb9933a8c7
75ks <kenta.s705@gmail.com>
2022-04-24 20:29:34 +0900

    Vueコンポーネント名の命名規則をOFFに設定した

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off' // Vueコンポーネント名の命名規則をOFF
   }
 }
